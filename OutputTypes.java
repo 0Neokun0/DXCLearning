15c5af3ece2426f446d7ddea5a32fdec943ca778
Meher <nishantnaresh.meher@dxc.com>
2023-05-10 11:36:48 +0900

    OutputType File Commit

---
+++ OutputTypes.java
@@ -0,0 +1,12 @@
+// 出力の種類
+public class OutputTypes {
+    public static void main(String args[]) {
+        //　文章の出力
+        System.out.println("Hello World!"); // println文章午後は新しい行列
+        System.out.println("こんにちは！");
+        // 数字の出力
+        System.out.println(3);
+        System.out.print(3 + 7); // printで新しい行列にならない
+    }
+    }
+    
