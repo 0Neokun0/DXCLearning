029c21ed6935896dbf272b5c5114d058e286eed1
Meher <nishantnaresh.meher@dxc.com>
2023-05-10 17:02:13 +0900

    if..else statement

---
+++ if_statements.java
@@ -0,0 +1,25 @@
+public class if_statements {
+    public static void main(String[] args) {
+        // if Statement
+        int number = 10;
+        // checks if number is less than 0
+        if (number < 0) {
+            System.out.println("The number is negative.");
+        }
+        System.out.println("Statement outside if block");
+
+        // if...else Statement
+        int number1 = 20;
+        // checks if the number is greater than 10
+        if (number1 > 0) {
+            System.out.println("The number is positive");
+        }
+        // execute this block
+        // if number is not greater than 10
+        else {
+            System.out.println("The number is not possible.");
+        }
+        System.out.println("Statement outside if...else block.");
+    }
+
+}
