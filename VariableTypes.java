95eda4f30865aadb5ef1e75315dc8439aad73252
Meher <nishantnaresh.meher@dxc.com>
2023-05-10 11:59:19 +0900

    Variables and calculation@

---
+++ VariableTypes.java
@@ -0,0 +1,20 @@
+public class VariableTypes {
+    public static void main(String args[]) {
+        int x;
+        x = 26;
+        System.out.println(x);
+        String y;
+        y = "おはようございます！";
+        System.out.println(y);
+
+        int umare;
+        umare = 1980;
+        if (umare < 1989) {
+            System.out.println("º˜a¶‚Ü‚ê‚Å‚·");
+        }
+        umare = 1990;
+        if (umare >= 1989) {
+            System.out.println("•½¬¶‚Ü‚ê‚Å‚·");
+        }
+    }
+}
