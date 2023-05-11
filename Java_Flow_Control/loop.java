93d1cdc5f22ae064d978c14152160f9f57c4076a
Meher <nishantnaresh.meher@dxc.com>
2023-05-11 14:59:03 +0900

    Loop Statement

---
+++ Java_Flow_Control/loop.java
@@ -0,0 +1,20 @@
+package Java_Flow_Control;
+
+public class loop {
+    public static void main(String[] args) {
+
+        int n = 5;
+        // ループ
+        for (int i = 1; i <= n; ++i) {
+            System.out.println("Java 面白い");
+        }
+        System.out.println("---------------------------------");
+
+        int j = 1, m = 5;
+        // While ループ1から５
+        while (j <= m) {
+            System.out.println(j);
+            j++;
+        }
+    }
+}
